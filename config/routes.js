30feee74545fea8d3c9788c7d8d85d2a13573a8e
SilverFlame83 <imren_83@abv.bg>
2023-04-20 14:42:53 +0300

    error implemented

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,11 @@
 const authController = require("../controllers/authController");
 const homeController = require("../controllers/homeController");
 const adsController = require("../controllers/adsController");
+const errorController = require("../controllers/errorController");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
   app.use("/ads", adsController);
+  app.use("*", errorController);
 };
