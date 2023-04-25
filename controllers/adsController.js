e03a4f5652e72e7a051a5d392cf0661e61a64e6d
SilverFlame83 <imren_83@abv.bg>
2023-04-25 18:46:20 +0300

    edit added

--- controllers/adsController.js
+++ controllers/adsController.js
@@ -18,49 +18,80 @@ router.post("/create", isUser(), async (req, res) => {
 
     await req.storage.createAd(adsData);
 
-    res.redirect('/')
+    res.redirect("/ads/catalog");
   } catch (err) {
     console.log(err.message);
 
     const ctx = {
-        errors: parseError(err),
-        adsData:{
-            headline: req.body.headline,
-            location: req.body.location,
-            companyName: req.body.companyName,
-            companyDescription: req.body.companyDescription
-        }
-    }
-    res.render('create', ctx)
+      errors: parseError(err),
+      adsData: {
+        headline: req.body.headline,
+        location: req.body.location,
+        companyName: req.body.companyName,
+        companyDescription: req.body.companyDescription,
+      },
+    };
+    res.render("create", ctx);
   }
 });
 
-router.get('/catalog', async(req,res)=>{
+router.get("/catalog", async (req, res) => {
   const ads = await req.storage.getAllAds();
 
-  res.render('catalog', {ads})
-})
+  res.render("catalog", { ads });
+});
 
-router.get('/details/:id', isUser(), async(req,res)=>{
+router.get("/details/:id", isUser(), async (req, res) => {
   const ad = await req.storage.getAdById(req.params.id);
 
-
   ad.isOwner = req.user && req.user._id == ad.owner._id;
-  ad.applied = req.user && ad.usersApplied.find((u) => u == req.user._id)
+  ad.applied = req.user && ad.usersApplied.find((u) => u == req.user._id);
 
-  res.render('details', {ad});
-})
+  res.render("details", { ad });
+});
 
-router.get('/apply/:id', isUser(), async(req,res)=>{
-  try{
-  
+router.get("/apply/:id", isUser(), async (req, res) => {
+  try {
     await req.storage.applyAdd(req.params.id, req.user._id);
 
-    res.redirect('/ads/details/' + req.params.id)
-  }catch(err){
-      console.log('Error', err.message);
-      res.redirect('/ads/details/' + req.params.id)
+    res.redirect("/ads/details/" + req.params.id);
+  } catch (err) {
+    console.log("Error", err.message);
+    res.redirect("/ads/details/" + req.params.id);
   }
-})
+});
+
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const ad = await req.storage.getAdById(req.params.id);
+
+    res.render("edit", { ad });
+  } catch (err) {
+    console.log(err.message);
+    res.redirect("/ads/details/" + req.params.id);
+  }
+});
+
+router.post("/edit/:id", isUser(), async (req, res) => {
+  try {
+    await req.storage.editAd(req.params.id, req.body);
+
+    res.redirect("/ads/details/" + req.params.id);
+  } catch (err) {
+    const ctx = {
+      errors: parseError(err),
+      ad: {
+        _id: req.params.id,
+        headline: req.body.headline,
+        location: req.body.location,
+        companyName: req.body.companyName,
+        companyDescription: req.body.companyDescription,
+        owner: req.body.owner,
+      },
+    };
+
+    res.render('edit', ctx)
+  }
+});
 
 module.exports = router;
